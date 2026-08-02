95c453a64effa6320ad5c736b12c9a6906b49a4a
fsalfner-dev <dev@felix.salfner.de>
2026-08-02 16:57:51 +0200

    introduce DEBUG mode and add pagination test (ignored)

--- tests/integration/SearchServiceFilesIntegrationTest.php
+++ tests/integration/SearchServiceFilesIntegrationTest.php
@@ -23,6 +23,10 @@ class SearchServiceFilesIntegrationTest extends TestCase {
      *   - all Nextcloud default files are deleted
      *   - the file and directories from tests/data are uploaded to Nextcloud
      *   - file sharing between the two users is set up
+     * 
+     * NOTE: The environment variable "NEXTCLOUD_URL" needs to be set to work properly
+     * 
+     * You can enable debug output with the environment variable DEBUG=true
      */
     protected function setUp(): void {
         parent::setUp();
@@ -45,7 +49,9 @@ class SearchServiceFilesIntegrationTest extends TestCase {
             foreach ($sharingSetup as $share) {
                 $this->helper->setupFileSharing($this->users[$share['sharer']], $this->users[$share['sharee']], $share['src'], $share['dest'] ?? null);
             }
-           echo "test setup successfully created with users " . $this->users[0]['user'] . " and " . $this->users[1]['user'] . ' ... ';
+            if (getenv('DEBUG') == 'true') {
+                echo "test setup successfully created with users " . $this->users[0]['user'] . " and " . $this->users[1]['user'] . ' ... \n';
+            }
         } catch (\Throwable $e) {
             throw $e;
         }
@@ -55,8 +61,10 @@ class SearchServiceFilesIntegrationTest extends TestCase {
      * remove the created test users
      */
     protected function tearDown(): void {
-#        $this->helper->deleteUser($this->users[0]);
-#        $this->helper->deleteUser($this->users[1]);
+        if (getenv('DEBUG') != 'true') {
+            $this->helper->deleteUser($this->users[0]);
+            $this->helper->deleteUser($this->users[1]);
+        }
         parent::tearDown();
     }
 
@@ -65,7 +73,7 @@ class SearchServiceFilesIntegrationTest extends TestCase {
      */
     public function testAllFiles(): void {
         // ---- tests for user 1
-        $result = $this->helper->makeSearchRequest($this->users[0], filename: '*', size: 100);
+        $result = $this->helper->makeSearchRequest($this->users[0], filename: '*', size: 100, sort: 'path', sort_order: 'asc');
 
         // Check if the total number of hits is the expected value
         $this->assertEquals($result['hits'], 20);
@@ -120,5 +128,55 @@ class SearchServiceFilesIntegrationTest extends TestCase {
 
     }
 
+    /**
+     * Test pagination
+     */
+    public function testPagination(): void {
+        /* THERE SEEMS TO BE A BUG IN FILE SEARCH PAGINATION ... skip the test for now */
+        $this->markTestSkipped('Pagination does not work properly for files in Nextcloud');
+
+        // CHECK THE FIRST PAGE
+        $result = $this->helper->makeSearchRequest($this->users[0], filename: '*', size: 10, page:0, sort: 'path', sort_order: 'asc', dump: true);
+
+        // Check if the total number of hits is the expected value
+        $this->assertEquals($result['hits'], 10);
+
+        // Check if the first 10 files appear in the list
+        $names = array_column($result['files'], 'name');
+        $this->assertEqualsCanonicalizing( [
+            "file_user1_1.txt",
+            "file_user1_2.txt",
+            "folder1_user1/",
+            "folder1_user1/file_user1_f1_1.txt",
+            "folder1_user1/file_user1_f1_2.odt",
+            "folder1_user1/file_user1_f1_3.odt",
+            "folder1_user1/file_user1_f1_3.pdf",
+            "folder1_user1/folder1-1_with a very long name and spaces for_user1/",
+            "folder1_user1/folder1-1_with a very long name and spaces for_user1/file_user1_f1-1_1.bmp",
+            "folder1_user1/folder1-1_with a very long name and spaces for_user1/file_user1_f1-1_1.gif",
+         ], $names );
+
+        // CHECK THE SECOND PAGE
+        $result = $this->helper->makeSearchRequest($this->users[0], filename: '*', size: 10, page: 1, sort: 'path', sort_order: 'asc', dump: true);
+
+        // Check if the total number of hits is the expected value
+        $this->assertEquals($result['hits'], 10);
+
+        // Check if the first 10 files appear in the list
+        $names = array_column($result['files'], 'name');
+        $this->assertEqualsCanonicalizing( [
+            "folder1_user1/folder1-1_with a very long name and spaces for_user1/file_user1_f1-1_1.odg",
+            "folder1_user1/folder1-1_with a very long name and spaces for_user1/file_user1_f1-1_1.png",
+            "folder1_user1/folder1-1_with a very long name and spaces for_user1/file_user1_f1-1_1.tif",
+            "folder1_user1/folder1-2_with a shorter name_user1/",
+            "folder1_user1/folder1-2_with a shorter name_user1/file_user1_f1-2_1.pdf",
+            "folder1_user1/folder1-2_with a shorter name_user1/file_user1_f1-2_1.png",
+            "folder1_user1/folder1-2_with a shorter name_user1/file_user1_f1-2_1.svg",
+            "folder1_user1/folder1-2_with a shorter name_user1/file_user1_f1-2_1.tiff",
+            "folder1_user1/folder1-2_with a shorter name_user1/file_user1_f1-2_1.webp",
+            "file_user2_1.txt"        
+         ], $names );
+    }
+        
 }
                         
